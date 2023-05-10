3e681a27a2989ac6c448a08bc72cd2fc77bb968e
Otavio Fina <otaviofina@gmail.com>
2023-05-10 20:52:10 -0300

    vr. 1.0.4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,32 @@
 import BadgeTipo from './BadgeTipo.tsx'
 import './PokemonCard.css'
+import exemploPoke from '../assets/pokesprite-master/pokesprite-master/pokemon-gen7x/regular/ditto.png'
+
+import { motion } from 'framer-motion'
+import { useRef } from 'react'
 
 export default function PokemonCard() {
+
+    const refDrag = useRef(null)
+
     return (
         <section className="pokemon-card">
             <div className="info-card">
-                <BadgeTipo tipo={0}/>
-                <BadgeTipo tipo={13}/>
+                <span className='poke-numero'>Nº 001</span>
+                <h5>PUKEMO</h5>
+                <div id='tipo-container'>
+                    <BadgeTipo tipo={0}/>
+                    <BadgeTipo tipo={13}/>
+                </div>
+
+            </div>
+
+            <div ref={refDrag} className='pokemon-img'>
+                <motion.img drag dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }} 
+                dragConstraints={refDrag} 
+                whileTap={{ scale: 0.85 }}
+                src={exemploPoke}
+                ></motion.img>
             </div>
         </section>
     )
